088a3e34bdbca79c18d3ba1abae69a20d124c87d
Rudraksh Singh <rudrakshsin3006@gmail.com>
2025-09-03 15:47:05 +0530

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 // next.config.mjs
 const nextConfig = {
-    experimental: {
-      allowedDevOrigins: ['http://192.168.29.52:3000'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  experimental: {
+    allowedDevOrigins: ["http://192.168.29.52:3000"],
+  },
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  },
+};
+
+export default nextConfig;
